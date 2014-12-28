be2296b4e930c553674656d346e2132e6644b665
roffl <misorinak@gmail.com>
2014-12-28 20:54:39 +0300

    dropdown-menu-360

--- js/header.js
+++ js/header.js
@@ -1,36 +1,64 @@
 $(document).ready(function() {
 
+  function dropdownMenuTablet() {
+    $(".inner-dropdown-toggle").click(function(event) {
 
+      event.preventDefault;
 
-  // var headerHeight = $('header').outerHeight();
-
-  // var displayWidth = $(window).width();
-
-  $(".inner-dropdown-toggle").click(function(event) {
-
-    event.preventDefault;
-
-    if( $(".inner-list").hasClass('active') ){
+      if( $(".inner-list").hasClass('active') ){
 
 
 
-      $(".inner-list")
-        .removeClass('active')
-        .appendTo('.nav-holder');
+        $(".inner-list")
+          .removeClass('active')
+          .appendTo('.nav-holder');
 
-      $(".inner-dropdown-toggle").removeClass("open");
+        $(".inner-dropdown-toggle").removeClass("open");
 
-      $('.inner-nav').remove();
+        $('.inner-nav').remove();
 
-    } else{
+      } else{
 
-      $('header').after('<div class="inner-nav"></div>');
+        $('header').after('<div class="inner-nav"></div>');
 
-      $(".inner-list")
-        .addClass('active')
-        .appendTo('.inner-nav');
+        $(".inner-list")
+          .addClass('active')
+          .appendTo('.inner-nav');
 
-        $(".inner-dropdown-toggle").addClass("open");
-    }
-  });
+          $(".inner-dropdown-toggle").addClass("open");
+      }
+    });
+  }
+
+
+
+  function dropdownMenuPhone() {
+
+    $('.outer-dropdown-toggle').click(function (event) {
+      event.preventDefault();
+
+      if( $(".outer-list").hasClass('outer-active') ){
+
+        $(".outer-list")
+          .removeClass('outer-active')
+          .appendTo('.nav-portfolio');
+
+        $('.inner-nav').remove();
+
+      } else{
+
+        $('header').after('<div class="inner-nav"></div>');
+
+        $(".outer-list")
+          .addClass('outer-active')
+          .appendTo('.inner-nav');
+      }
+    });
+  }
+
+
+
+  dropdownMenuPhone();
+  dropdownMenuTablet();
+
 });
