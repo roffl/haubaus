00d5b48d97d9f5038463905ef9ee7a4338e1541e
roffl <misorinak@gmail.com>
2015-01-02 02:52:35 +0300

    footer-360-stylised

--- js/footer.js
+++ js/footer.js
@@ -1,10 +1,33 @@
 $(document).ready(function() {
-    $(window).resize(function(){
-        var contacts = $('.block-contacts');
-        if( $(window).width() < 770 ){
-            $('.block-about:first').after(contacts);
-        } else{
-            
-        }
-    });
+
+    function footerStructure(){
+        $(window).resize(function(){
+            var contacts = $('.block-contacts');
+            if ( $(window).width() < 1200 && $(window).width() > 770 ){
+                $('.block-about:first').before(contacts);
+            } else if( $(window).width() < 770 ){
+                $('.block-about:first').after(contacts);
+            } else{
+                $('.block-about:last').after(contacts);
+            }
+        });
+    }
+
+    function contactsStructure(){
+        $(window).resize(function() {
+            var address = $('.address');
+            var phone = $('.phone');
+            var email = $('.email');
+            var social = $('.middle-footer .social')
+            var copy = $('.copy');
+            if( $(window).width() < 770 ){
+                $(address).before(phone);
+                $(phone).after(email);
+                $(social).after(copy);
+            }
+        });
+    }
+
+    footerStructure();
+    contactsStructure();
 });
