80cf1f44dadc4730046cd0eb2ee2043b1a2f7a4a
roffl <misorinak@gmail.com>
2015-01-02 03:36:39 +0300

    footer-350-final

--- js/footer.js
+++ js/footer.js
@@ -1,33 +1,35 @@
 $(document).ready(function() {
 
     function footerStructure(){
-        $(window).resize(function(){
-            var contacts = $('.block-contacts');
-            if ( $(window).width() < 1200 && $(window).width() > 770 ){
-                $('.block-about:first').before(contacts);
-            } else if( $(window).width() < 770 ){
-                $('.block-about:first').after(contacts);
-            } else{
-                $('.block-about:last').after(contacts);
-            }
-        });
+        var contacts = $('.block-contacts');
+        if ( $(window).width() < 1200 && $(window).width() > 770 ){
+            $('.block-about:first').before(contacts);
+        } else if( $(window).width() < 770 ){
+            $('.block-about:first').after(contacts);
+        } else{
+            $('.block-about:last').after(contacts);
+        }
     }
 
     function contactsStructure(){
-        $(window).resize(function() {
-            var address = $('.address');
-            var phone = $('.phone');
-            var email = $('.email');
-            var social = $('.middle-footer .social')
-            var copy = $('.copy');
-            if( $(window).width() < 770 ){
-                $(address).before(phone);
-                $(phone).after(email);
-                $(social).after(copy);
-            }
-        });
+        var address = $('.address');
+        var phone = $('.phone');
+        var email = $('.email');
+        var social = $('.middle-footer .social')
+        var copy = $('.copy');
+        if( $(window).width() < 770 ){
+            $(address).before(phone);
+            $(phone).after(email);
+            $(social).after(copy);
+        } else{
+            $(phone).before(address);
+        }
     }
 
     footerStructure();
     contactsStructure();
+    $(window).resize(function() {
+        footerStructure();
+        contactsStructure();
+    });
 });
